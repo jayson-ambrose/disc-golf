50cc8693852c559ddb550d5b6d3faaa827807045
Eric Thvedt <ericsthvedt@gmail.com>
2023-04-18 14:55:30 -0700

    models

--- server/models.py
+++ server/models.py
@@ -21,21 +21,21 @@ class User(DefaultBase):
     __tablename__ = 'users'
 
     username = db.Column(db.String, unique=True)
-    _password_hash = db.Column(db.String, nullable=False)
+    _password = db.Column(db.String, nullable=False)
 
     players = db.relationship('Player', backref='user')
 
     @hybrid_property
-    def password_hash(self):
-        return self._password_hash
+    def password(self):
+        return self._password
     
-    @password_hash.setter
-    def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
+    @password.setter
+    def password(self, password):
+        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        self._password = password_hash.decode('utf-8')
 
     def auth(self, password):
-        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+        return bcrypt.check_password_hash(self.password, password)
 
 class Player(DefaultBase):
     __tablename__ = 'players'
