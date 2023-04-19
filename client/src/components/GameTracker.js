9db04da6528b75e339722a4d29d5eb84f34e3bc9
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-04-19 10:44:07 -0600

    various changes

--- client/src/components/GameTracker.js
+++ client/src/components/GameTracker.js
@@ -13,8 +13,51 @@ function GameTracker() {
 
     ])
 
+    function getPlayers () {
+
+        fetch(`/rounds/${id}/players`)
+        .then(resp => resp.json())
+        .then(data => setPlayers(...players , [
+            {id1: data.player1.id},
+            {id2: data.player2.id},
+            {id3: data.player3.id},
+            {id4: data.player4.id}
+        ]))
+    }
+
     const startNewGameForm = (
-        <NewGameForm />
+        <div>
+            <h1>Start a New Round</h1>
+            <form>                
+                <table>
+                    <tr>
+                        <td><label>Course ID</label></td>
+                        <td><input type='text' name='course_id'></input></td>
+                    </tr>                
+                    <tr>
+                        <td><label>Tournament ID</label></td>
+                        <td><input type='text' name='tournament_id'></input></td>
+                    </tr>                
+                    <tr>
+                        <td><label>Player 1 Name</label></td>
+                        <td><input type='text' name='p1_name'></input></td>
+                    </tr>  
+                    <tr>
+                        <td><label>Player 2 Name</label></td>
+                        <td><input type='text' name='p2_name'></input></td>
+                    </tr>  
+                    <tr>
+                        <td><label>Player 3 Name</label></td>
+                        <td><input type='text' name='p3_name'></input></td>
+                    </tr>  
+                    <tr>
+                        <td><label>Player 4 Name</label></td>
+                        <td><input type='text' name='p4_name'></input></td>
+                    </tr>  
+                </table><br/>
+            <button type='submit'>Start Round</button> 
+            </form><br/>
+        </div>
     )
 
     const gameProcess = (
